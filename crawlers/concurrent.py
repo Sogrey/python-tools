f34fb2a42cf7ed4d9b127a1065a8275b60781fea
sogrey <suzhaoheng2006@163.com>
2022-05-19 00:24:34 +0800

    💖 deploy main branch.

--- crawlers/concurrent.py
+++ crawlers/concurrent.py
@@ -2,6 +2,8 @@ import re
 import requests
 import hashlib
 import time
+from concurrent.futures import ThreadPoolExecutor
+p = ThreadPoolExecutor(30)
 
 
 def get_index(url):
@@ -11,24 +13,25 @@ def get_index(url):
 
 
 def parse_index(res):
+    res = res.result()
     urls = re.findall(r'class="item".*?href="(.*?)"',
                       res, re.S)  # re.S 把文本信息转为一行匹配
-    return urls
-
-
-def get_detail(urls):
     for url in urls:
-        if not url.startswith('http'):
-            url = 'http://www.xiaohuar.com%s' % url
+        p.submit(get_detail(url))
 
-        result = requests.get(url)
-        if result.status_code == 200:
-            mp4_url_list = re.findall(
-                r'id="media".?src="(.*?)"', result.text, re.S)
-            if mp4_url_list:
-                mp4_url = mp4_url_list[0]
-                print(mp4_url)
-                # save(mp4_url)
+
+def get_detail(url):
+    if not url.startswith('http'):
+        url = 'http://www.xiaohuar.com%s' % url
+
+    result = requests.get(url)
+    if result.status_code == 200:
+        mp4_url_list = re.findall(
+            r'id="media".?src="(.*?)"', result.text, re.S)
+        if mp4_url_list:
+            mp4_url = mp4_url_list[0]
+            print(mp4_url)
+            # save(mp4_url)
 
 
 def save(url):
@@ -45,9 +48,8 @@ def save(url):
 
 def main():
     for i in range(5):
-        res1 = get_index('http://www.xiaohuar.com/list-3-%s.html' % i)
-        res2 = parse_index(res1)
-        get_detail(res2)
+        p.submit(get_index, 'http://www.xiaohuar.com/list-3-%s.html' %
+                 i).add_done_callback(parse_index)
 
 
 if __name__ == '__main__':
